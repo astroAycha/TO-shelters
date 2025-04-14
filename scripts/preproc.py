8d644dd1876c8265ffd4a0fa1e17075f7b95828a
astroAycha <aycha.tammour@gmail.com>
2025-04-14 11:34:55 -0400

    added a new function to extend the date for the forecasting week

--- scripts/preproc.py
+++ scripts/preproc.py
@@ -1,5 +1,9 @@
 import pandas as pd
 import numpy as np
+from datetime import datetime, timedelta
+import logging
+
+logging.basicConfig(filename='shelter_data.log', level=logging.INFO)
 
 
 
@@ -65,6 +69,75 @@ def create_lag_features(data: pd.DataFrame) -> pd.DataFrame:
 
 
     return data
+
+
+def combine_data(data_path: str) -> pd.DataFrame:
+
+
+    data2022 = pd.read_csv(f"{data_path}shelter_capacity_2022.csv",
+                           parse_dates=['OCCUPANCY_DATE'],
+                           date_format='%y-%m-%d')
+    data2022['OCCUPANCY_DATE'] = pd.to_datetime(data2022['OCCUPANCY_DATE']).dt.strftime('%Y-%m-%d')
+    data2023 = pd.read_csv(f"{data_path}shelter_capacity_2023.csv")
+    data2023['OCCUPANCY_DATE'] = pd.to_datetime(data2023['OCCUPANCY_DATE']).dt.strftime('%Y-%m-%d')
+    data2024 = pd.read_csv(f"{data_path}shelter_capacity_2024.csv")
+    data2024['OCCUPANCY_DATE'] = pd.to_datetime(data2024['OCCUPANCY_DATE']).dt.strftime('%Y-%m-%d')
+    data2025 = pd.read_csv(f"{data_path}shelter_capacity_2025.csv")    
+    data2025['OCCUPANCY_DATE'] = pd.to_datetime(data2025['OCCUPANCY_DATE']).dt.strftime('%Y-%m-%d')
+
+
+
+    # first stack the shelter data
+    shelter_capacity_data = pd.concat([data2022, data2023, data2024, data2025],
+                                      ignore_index=True)
+
+    packed_shelter_data = shelter_capacity_data.groupby(['SHELTER_ID', 
+                                                     'LOCATION_ID',
+                                                     'ORGANIZATION_NAME',
+                                                     'SHELTER_GROUP',
+                                                     'LOCATION_NAME',
+                                                     'OCCUPANCY_DATE']).agg({'OCCUPANCY_RATE_BEDS': 'mean'})
+    
+    shelter_data = packed_shelter_data.reset_index()
+
+    shelter_data['OCCUPANCY_DATE'] = pd.to_datetime(shelter_data['OCCUPANCY_DATE'],
+                                                    format='mixed')
+
+
+    # add rows for an additional week for forecasting:
+    start = shelter_data['OCCUPANCY_DATE'].max().date() 
+    end = start + pd.Timedelta(7, 'D')
+    logging.info(f"adding rows for dates: {start}", f"\n end date: {end}")
+
+    new_week_dates = pd.date_range(start, end, freq='D')
+    new_week = pd.DataFrame(columns=shelter_data.columns)
+
+    new_week['OCCUPANCY_DATE'] = new_week_dates
+
+    # stack new week on the shelter data frame:
+    shelter_data_extended = pd.concat([shelter_data, new_week])
+
+    # read weather data for Toronto
+    weather_data = pd.read_csv(f"{data_path}daily_weather_data.csv")
+    print(f">>> weather data size: {weather_data.shape}")
+    
+    weather_data['OCCUPANCY_DATE'] = pd.to_datetime(weather_data['date']).dt.date.astype('datetime64[ns]')
+
+    full_data = shelter_data_extended.merge(weather_data, on='OCCUPANCY_DATE', how='left')
+
+    today_date = datetime.today()
+    print(f">>> Today is: {today_date}")
+
+    full_data = full_data[
+                        # (full_data['OCCUPANCY_DATE'] < today_date) 
+                    #   & 
+                      (full_data['OCCUPANCY_DATE'] > today_date - timedelta(days=1200))
+                      ]
+
+    full_data.to_csv(f"{data_path}daily-shelter-overnight-capacity.csv",
+                   index=False)
+
+    # return full_data
 
 # def create_window_features(data: pd.DataFrame) -> pd.DataFrame:
 
